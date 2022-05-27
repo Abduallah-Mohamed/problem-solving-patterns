e586d150ca6080b8fee32bc43a6df7e21e646958
Abduallah-Mohamed <Abduallahmohamed966@gmail.com>
2022-05-28 00:13:12 +0200

    better solution for unCommonWords problem

--- frequency pattern/unCommonWords.js
+++ frequency pattern/unCommonWords.js
@@ -1,31 +1,29 @@
 var uncommonFromSentences = function (s1, s2) {
+  if (s2.length === 0 && s1.length !== 0) return s1.split(" ");
+  if (s1.length === 0 && s2.length !== 0) return s2.split(" ");
+  if (s1.length === 0 && s2.length === 0) return [];
+
   s1 = s1.split(" ");
   s2 = s2.split(" ");
 
   const fc1 = {};
-  const fc2 = {};
 
   const result = [];
 
+  // insert the whole words into the fc1 object
   for (let word of s1) {
     fc1[word] = fc1[word] + 1 || 1;
   }
 
+  // inserthe whole words into the fc1 object too
   for (let word of s2) {
-    fc2[word] = fc2[word] + 1 || 1;
+    fc1[word] = fc1[word] + 1 || 1;
   }
 
-  for (let word of s2) {
-    if (!fc1[word]) {
+  // if any word in fc1 has value of 1 it will be the different word in the both sentences
+  for (let word in fc1) {
+    if (fc1[word] === 1) {
       result.push(word);
-    }
-  }
-
-  for (let word of s1) {
-    if (!fc2[word]) {
-      if (!result.includes(word)) {
-        result.push(word);
-      }
     }
   }
 
